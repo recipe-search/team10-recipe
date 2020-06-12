50d7113b6f99965fcaf4dee503e0ce7a5f2e9f66
Beni Adi Pranata <beniadipranata@gmail.com>
2020-06-12 11:08:46 +0700

    All fix

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,7 +1,28 @@
 const { User } = require('../models');
+const sendgrid = require('@sendgrid/mail');
 
 class EmailController {
-    static send(req, res, next) {}
+    static send(req, res, next) {
+        sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+        const message = {
+            to: req.currentUserEmail,
+            from: 'no-reply@bap.my.id',
+            subject: req.body.subject,
+            html: req.body.message,
+        };
+        console.log(message);
+        sendgrid.send(message).then(
+            () => {
+                console.log(`Email sent to ${message.to}`);
+                res.status(200).json({ message: 'success' });
+            },
+            (error) => {
+                if (error.response) {
+                    next({ name: '' });
+                }
+            }
+        );
+    }
 }
 
 module.exports = EmailController;
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,37 +1,36 @@
-"use strict";
+'use strict';
 
-const { verifyToken } = require("../helpers/jwt");
-const { User } = require("../models");
+const { verifyToken } = require('../helpers/jwt');
+const { User } = require('../models');
 
 const authentication = (req, res, next) => {
-  let token = req.headers.token;
+    let token = req.headers.token;
 
-  try {
-    let decoded = verifyToken(token);
-    let { id } = decoded;
+    try {
+        let decoded = verifyToken(token);
+        let { id, email } = decoded;
 
-    User.findByPk(id)
-      .then((result) => {
-        if (result) {
-          req.currentUserId = id;
-          next();
-        } else {
-          throw {
-            msg: "Please login first",
-            code: 401,
-          };
-        }
-      })
-      .catch((err) => {
-        next(err);
-      });
-  } catch (error) {
-    next(error);
-  }
+        User.findByPk(id)
+            .then((result) => {
+                if (result) {
+                    req.currentUserId = id;
+                    req.currentUserEmail = email;
+                    next();
+                } else {
+                    throw {
+                        msg: 'Please login first',
+                        code: 401,
+                    };
+                }
+            })
+            .catch((err) => {
+                next(err);
+            });
+    } catch (error) {
+        next(error);
+    }
 };
 
-const emailAuthorization = (req, res, next) => {
-    
-};
+const emailAuthorization = (req, res, next) => {};
 
 module.exports = { authentication, emailAuthorization };
